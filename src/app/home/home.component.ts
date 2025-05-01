c01ca8997999470247be11e5b9197d3de8eba1c0
Sai Charan <kutikuppalasaicharan97@gmail.com>
2025-05-01 23:44:40 +0530

    added theme option

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,19 @@
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, NgClass],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  isDarkMode = false;
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    document.body.classList.toggle('dark-theme');
+  }
 }
